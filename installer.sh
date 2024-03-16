6be9b965cde898aa0323225018b15619793eebfc
Bijaya Tamang <155700828+BijayaTamangShyangtan@users.noreply.github.com>
2024-03-16 08:24:30 +0545

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 pkg install -y root-repo 
 pkg install -y git tsu python wpa-supplicant pixiewps iw
 
-git clone https://github.com/W8SOJIB/W8RootWifiHK
+git clone https://github.com/BijayaTamangShyangtan/BJROOTWIFIHK/tree/main
 
 chmod +x BJROOTWIFIHK/oneshot.py
 
